f841f03e595ef58906fa35dee47780afdc06d135
Ben Smith <benjaminsmith1981@googlemail.com>
2022-05-03 17:08:42 +0000

    Fixed connecting to database

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def hello():
     return "hi world"
 
-if __name_ == "__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
