e6b936f42befc35512dbc8cb2943bd2ce0b4943f
Ben Smith <benjaminsmith1981@googlemail.com>
2022-05-11 19:06:22 +0000

    Side bar and nav

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
     import env
@@ -19,6 +20,10 @@ mongo = PyMongo(app)
 def get_classifieds():
     classifieds = mongo.db.classifieds.find()
     return render_template("tasks.html", classifieds=classifieds)
+
+@app.route("/register", methods["GET","POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
