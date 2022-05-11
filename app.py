fd97135994c8ac18b33e939b61fc1f0d6b0b8c66
Ben Smith <benjaminsmith1981@googlemail.com>
2022-05-11 19:55:47 +0000

    added usrename check

--- app.py
+++ app.py
@@ -23,6 +23,20 @@ def get_classifieds():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if request.method == "POST":
+            #check user name exist
+            existing_user = mongo.db.users.find_one(
+                {"username":request.form.get("username").lower()})
+            if existing_user:
+                flash("Username exists")
+                return redirect(url_for("register"))
+            register =  {
+                "username":request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            # mongo.db.users.insert_one(register)
+            session["user"] = request.form.get("username").lower()
+            flash("Registration Successful!")
     return render_template("register.html")
 
 if __name__ == "__main__":
